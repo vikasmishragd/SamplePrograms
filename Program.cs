263717a1e26442747304109dc7011c32f4132805
vikasmishragd <65272228+vikasmishragd@users.noreply.github.com>
2020-11-27 15:05:28 +0530

    Printing Even odd numbers in sync using reset Event
    Sync two threads using AutoSetEvent and signaling mechanism

---
+++ Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ConsoleApp7
+{
+	class Program
+	{
+		public static AutoResetEvent even;
+		public static AutoResetEvent odd;
+		static void Main(string[] args)
+		{
+			even = new AutoResetEvent(true);
+			odd = new AutoResetEvent(false);
+
+			Thread T1 = new Thread(PrintEven);
+			Thread T2 = new Thread(PrintOdd);
+			T1.Start();
+			T2.Start();
+			T1.Join();
+			T2.Join();
+			Console.Read();
+
+		}
+
+		private static void PrintOdd()
+		{
+			for(int i=1;i<20;i=i+2)
+			{
+				odd.WaitOne();
+				Console.WriteLine(" "+i);
+				even.Set();
+			}
+		}
+
+		private static void PrintEven()
+		{
+			for (int i = 0; i < 20; i = i + 2)
+			{
+				even.WaitOne();
+				Console.WriteLine(i);
+				Thread.Sleep(3000);
+				odd.Set();
+			}
+		}
+	}
+}
